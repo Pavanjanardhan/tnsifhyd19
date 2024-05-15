0b7f28724f948e4083324d2ee8e5b3ddfce20b64
Pavanjanardhan <168440032+Pavanjanardhan@users.noreply.github.com>
2024-05-15 12:41:20 +0530

    cricket.java

---
+++ cricket.java
@@ -0,0 +1,17 @@
+package com.sif.solve;
+
+public class cricket {
+	String batsman="viratkohli";
+	static String bowler="siraj";
+	void display() {
+	System.out.println("hii virat");
+	}
+	static String display1() {
+		return "hey";
+		
+	}
+	
+	
+		// TODO Auto-generated method stub
+
+	}
